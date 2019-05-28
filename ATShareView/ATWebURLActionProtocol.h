dd4fa7447e25900b818e61392dcb386347a97038
ablett <ablett.chen@gmail.com>
2019-05-28 22:43:16 +0800

    mod

---
+++ ATShareView/ATWebURLActionProtocol.h
@@ -0,0 +1,16 @@
+//
+//  ATWebURLActionProtocol.h
+//  ATPopupView
+//
+//  Created by ablett on 2019/5/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol ATWebURLActionProtocol <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
